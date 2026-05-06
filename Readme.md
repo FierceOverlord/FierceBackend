2f1cf2a0aa458424876de7c98217ac1a87dc19dd
FierceOverlord <vermarishav410@gmail.com>
2026-05-06 21:54:12 +0530

    custom api response & error handling

--- Readme.md
+++ Readme.md
@@ -1,1 +1,15 @@
 # Fierce's Backend
+
+First structured the project: 
+
+1. npm init for initialising node pacakge manager, configured package.json for "type": "module".
+
+2. Created necessary folders - public, src, inside src - controllers, db, middlewares, models, utils, routes and files - db/index.js, root - app.js, constants.js, index.js, .env, .gitignore for the project.
+
+3. Installed dev dependencies - prettier, nodemon.
+
+4. Created Database on MongoDB atlas which provides cloud DB.
+
+5. Installed dotenv, express and mongoose.
+
+6. Connected to the database.
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv"
 import mongoose from "mongoose";
-import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 
 dotenv.config({
@@ -9,3 +9,51 @@ dotenv.config({
 })
 
 connectDB()
+.then(() => {
+
+    app.on("error", (error) => {
+        console.log("ERROR: ", error);
+        throw error
+    })
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+        
+    })
+})
+.catch((err) => {
+    console.log("MongoDB Connection FAILED : ", err)
+})
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+const app = express()
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("errror", (error) => {
+            console.log("ERRR: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err
+    }
+})()
+
+*/
